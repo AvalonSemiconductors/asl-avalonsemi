ff39bb1fbd992fe8b015ffca39997b2538512852
Tholin <cutie@tholin.dev>
2024-11-28 13:54:20 +0100

    Add support for LVDC.

---
+++ codelvdc.c
@@ -0,0 +1,226 @@
+/*
+ * (Tholin, 27/09/2024) Added support for LVDC
+ */
+
+#include "stdinc.h"
+#include <ctype.h>
+#include <string.h>
+
+#include "bpemu.h"
+#include "strutil.h"
+#include "asmdef.h"
+#include "asmsub.h"
+#include "asmpars.h"
+#include "asmitree.h"
+#include "codevars.h"
+#include "headids.h"
+#include "intpseudo.h"
+#include "errmsg.h"
+
+#include "codelvdc.h"
+
+static CPUVar CPUlvdc;
+
+#define FLAG_RESIDUAL_ALLOWED 128
+#define FLAG_IMM_VALUE 256
+
+static void DecodeSimple(Word Code) {
+	if(ChkArgCnt(1, 1)) {
+		Boolean OK;
+		Boolean residual = False;
+		if(ArgStr[1].str.p_str[0] == '+') {
+			if((Code & FLAG_RESIDUAL_ALLOWED) == 0) {
+				WrError(ErrNum_InvAddrMode);
+				return;
+			}
+			residual = True;
+
+		}
+		Word addr = EvalStrIntExpressionOffs(&ArgStr[1], residual, (Code & FLAG_IMM_VALUE) != 0 ? UInt9 : UInt8, &OK);
+		if((addr & 256) != 0) {
+			addr &= 0xFF;
+			residual = True;
+		}
+		Word fullInstr = 0;
+		if(OK) {
+			Code &= 0x0F;
+			fullInstr |= Code;
+			if(residual) fullInstr |= (1 << 4);
+			fullInstr |= addr << 5;
+			WAsmCode[0] = fullInstr;
+			CodeLen = 1;
+		}
+	}
+}
+
+static void DecodeCDS(Word Code) {
+	if(ChkArgCnt(1, 2)) {
+		Boolean OK;
+		if(ArgCnt == 1) {
+			if((Code & 2) == 0) {
+				WrError(ErrNum_InvAddrMode);
+				return;
+			}
+			Word setting = EvalStrIntExpression(&ArgStr[1], UInt8, &OK);
+			if(OK) {
+				Word fullInstr = setting << 5;
+				fullInstr |= 0xE;
+				WAsmCode[0] = fullInstr;
+				CodeLen = 1;
+			}
+		}else {
+			if((Code & 2) != 0) {
+				WrError(ErrNum_InvAddrMode);
+				return;
+			}
+			Word DM = EvalStrIntExpression(&ArgStr[1], UInt3, &OK);
+			if(OK) {
+				Word DS = EvalStrIntExpression(&ArgStr[2], UInt4, &OK);
+				if(OK) {
+					Word fullInstr = (DM << 1) | (DS << 4);
+					if((Code & 1) != 0) fullInstr |= 1;
+					fullInstr <<= 5;
+					fullInstr |= 0xE;
+					WAsmCode[0] = fullInstr;
+					CodeLen = 1;
+				}
+			}
+		}
+	}
+}
+
+static void DecodeShift(Word Code) {
+	if((Code & 2) != 0) {
+		//Clear-accumulator to zero is technically a shift instruction
+		Word fullInstr = 0x1E;
+		WAsmCode[0] = fullInstr;
+		CodeLen = 1;
+		return;
+	}
+	if(ChkArgCnt(1, 1)) {
+		Boolean OK;
+		Word shiftBy = EvalStrIntExpression(&ArgStr[1], UInt4, &OK);
+		if(shiftBy > 2 || shiftBy == 0) {
+			WrError(ErrNum_InvOpSize);
+			return;
+		}
+		Word fullInstr = 0x1E;
+		if((Code & 1) != 0) {
+			//Right-shift
+			if(shiftBy == 1) fullInstr |= (1 << 5);
+			else if(shiftBy == 2) fullInstr |= (1 << 6);
+		}else {
+			//Left-shift
+			if(shiftBy == 1) fullInstr |= (1 << 9);
+			else if(shiftBy == 2) fullInstr |= (1 << 10);
+		}
+		WAsmCode[0] = fullInstr;
+		CodeLen = 1;
+	}
+}
+
+static void DecodeEXM(Word Code) {
+	if(ChkArgCnt(3, 3)) {
+		Boolean OK;
+		Word adr = EvalStrIntExpression(&ArgStr[1], UInt2, &OK);
+		if(OK) {
+			Word syl = EvalStrIntExpression(&ArgStr[2], UInt1, &OK);
+			if(OK) {
+				Word mod = EvalStrIntExpression(&ArgStr[3], UInt4, &OK);
+				Word fullInstr = 0x101E;
+				fullInstr |= (mod << 5);
+				if(syl != 0) fullInstr |= (1 << 9);
+				fullInstr |= (adr << 10);
+				WAsmCode[0] = fullInstr;
+				CodeLen = 1;
+			}
+		}
+	}
+}
+
+static void AddSimple(char *NName, Word NCode, Boolean residualAllowed) {
+	if(residualAllowed) NCode |= FLAG_RESIDUAL_ALLOWED;
+	AddInstTable(InstTable, NName, NCode, DecodeSimple);
+}
+
+static void AddCDS(char *NName, Word NCode) {
+	AddInstTable(InstTable, NName, NCode, DecodeCDS);
+}
+
+static void AddShift(char *NName, Word NCode) {
+	AddInstTable(InstTable, NName, NCode, DecodeShift);
+}
+
+static void InitFields(void) {
+	InstTable = CreateInstTable(24);
+	
+	AddSimple("HOP", 0x0, True);
+	AddSimple("MPY", 0x1, True);
+	AddSimple("SUB", 0x2, True);
+	AddSimple("DIV", 0x3, True);
+	AddSimple("TNZ", 0x4, True);
+	AddSimple("MPH", 0x5, True);
+	AddSimple("AND", 0x6, True);
+	AddSimple("ADD", 0x7, True);
+	AddSimple("TRA", 0x8, True);
+	AddSimple("XOR", 0x9, True);
+	AddSimple("PIO", 0xA | FLAG_IMM_VALUE, False);
+	AddSimple("STO", 0xB, True);
+	AddSimple("TMI", 0xC, True);
+	AddSimple("RSU", 0xD, True);
+	AddSimple("CLA", 0xF, True);
+
+	AddCDS("CDS", 2);
+	AddCDS("CDSD", 1);
+	AddCDS("CDSS", 0);
+
+	AddShift("CL", 2);
+	AddShift("SHL", 0);
+	AddShift("SHR", 1);
+
+	AddInstTable(InstTable, "EXM", 0, DecodeEXM);
+}
+
+static void DeinitFields(void) {
+	DestroyInstTable(InstTable);
+}
+
+static void MakeCode_lvdc(void) {
+	CodeLen = 0; DontPrint = False;
+	if((*OpPart.str.p_str == '\0') && (ArgCnt == 0)) return;
+	if(DecodeIntelPseudo(False)) return;
+	if(Memo("")) return;
+	if(!LookupInstTable(InstTable, OpPart.str.p_str)) WrStrErrorPos(ErrNum_UnknownInstruction, &OpPart);
+}
+
+static Boolean IsDef_lvdc(void) {
+	return FALSE;
+}
+
+static void SwitchFrom_lvdc(void) {
+	DeinitFields();
+}
+
+static void SwitchTo_lvdc(void) {
+	const TFamilyDescr *pDescr = FindFamilyByName("LVDC");
+	SetIntConstMode(eIntConstModeMoto);
+	TurnWords = False;
+	ShiftIsOccupied = False;
+	PCSymbol = "$";
+	HeaderID = pDescr->Id;
+	DivideChars = ",";
+	HasAttrs = False;
+	
+	ValidSegs = (1 << SegCode);
+	Grans[SegCode] = 2; ListGrans[SegCode] = 2; SegInits[SegCode] = 0;
+	SegLimits[SegCode] = 0x3fffl;
+	
+	MakeCode = MakeCode_lvdc;
+	IsDef = IsDef_lvdc;
+	SwitchFrom = SwitchFrom_lvdc;
+	InitFields();
+}
+
+void code_lvdc_init(void) {
+	CPUlvdc = AddCPU("LVDC", SwitchTo_lvdc);
+}
---
+++ codelvdc.h
@@ -0,0 +1,8 @@
+/*
+ * (Tholin, 27/09/2024) Added support for LVDC
+ */
+
+#ifndef _CODELVDC_H
+#define _CODELVDC_H
+extern void code_lvdc_init(void);
+#endif
